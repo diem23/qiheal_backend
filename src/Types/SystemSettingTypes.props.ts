73ffed09b3663b64d4c44a38f637aba47c076d27
diem23 <nmdiem23@gmail.com>
2025-06-13 10:30:30 +0700

    feat: implement SystemSettings CRUD operations and add SystemSettingRouter for API endpoints

--- src/Types/SystemSettingTypes.props.ts
+++ src/Types/SystemSettingTypes.props.ts
@@ -7,28 +7,8 @@ export enum SystemSettingName {
     LANGUAGE = "language",
     SITE_SETTINGS = "siteSettings"
 }
-export interface BaseSystemSetting<TName, TValue> {
-    _id: Types.ObjectId
-    name: TName;
-    value: TValue;
-}
 
-export type PointConversion = BaseSystemSetting<
-    SystemSettingName.POINT_CONVERSION,
-    {
+export type PointConversionValue = {
         pointsPerDiscount: number;
         moneyPerDiscount: number;
     }
->;
-
-// Example for another setting
-export type CurrencySetting = BaseSystemSetting<
-    SystemSettingName.CURRENCY,
-    {
-        currencyCode: string;
-        symbol: string;
-    }
->;
-
-// Union type for all settings
-export type SystemSetting = PointConversion | CurrencySetting; // | ...other settings
